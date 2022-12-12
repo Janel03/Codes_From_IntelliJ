b198fbc1aff7b9862d1673bcc711147d32cc4350
Janel03 <s.zhanel@gmail.com>
2022-12-11 23:38:52 -0500

    Test2

---
+++ JavaExercises/src/CodeCademy.java
@@ -0,0 +1,16 @@
+public class CodeCademy {
+    public static void main(String[] args) {
+
+        int myNumber=16;//original number
+        int stepOne=myNumber*myNumber;
+        int stepTwo=stepOne+myNumber;
+        int stepThree=stepTwo/myNumber;
+        int stepFour=stepThree+17;
+        int stepFive=stepFour-myNumber;
+        int stepSix=stepFive/6;
+        System.out.println(stepSix);
+        //change the original number value and same result comes out
+
+    }
+}
+
